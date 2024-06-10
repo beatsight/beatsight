fe07140209088aa1fe5ce47f9174e6d72b9c5dcb
zheng xie <xiez1989@gmail.com>
2024-06-11 07:00:44 +0800

    feat(web): add fonts

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,24 @@
 import Welcome from "./components/Welcome"
+import styles from './ui/home.module.css';
 
 export default function App() {
   return (
       <>
       <Welcome />
+
+      <div
+        className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+        />
+
+      <div className={styles.shape} />
+
+      
+      <p className="font-['Lusitana'] ">
+        This is with Font Link. We are linking the fonts from the Google Fonts.
+        </p>
+
       </>
   );
+
 }
+
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+// import './fonts/Lusitana-Bold.ttf'
+// import './fonts/Lusitana-Regular.ttf'
 // import './fonts.css'
+
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
 )
 
